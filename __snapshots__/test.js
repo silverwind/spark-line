04cb02d474d74bfb775a19ea5281e095fb1f1f97
silverwind <me@silverwind.io>
2020-03-31 09:13:53 +0200

    update snapshots and eslint-ignore them

--- __snapshots__/test.js
+++ __snapshots__/test.js
@@ -15,7 +15,7 @@ exports['sparkz renders svg for 1-item array 1'] = `
 `
 
 exports['sparkz renders interactive svg 1'] = `
-<svg width="100" height="30" stroke-width="2"><path class="sparkz--fill" d="M4 26 L 4 26 L 50 15 L 96 4 V 30 L 4 30 Z" stroke="none"></path><path class="sparkz--line" d="M4 26 L 4 26 L 50 15 L 96 4" fill="none"></path><line class="sparkz--cursor" x1="-1000" x2="-1000" y1="0" y2="30" stroke-width="2"></line><circle class="sparkz--spot" cx="-1000" cy="-1000" r="2"></circle><rect width="100" height="30" style="fill: transparent; stroke: transparent" class="sparkz--interaction-layer"></rect></svg>
+<svg width="100" height="30" stroke-width="2"><path class="sparkz--fill" d="M4 26 L 4 26 L 50 15 L 96 4 V 30 L 4 30 Z" stroke="none"></path><path class="sparkz--line" d="M4 26 L 4 26 L 50 15 L 96 4" fill="none"></path><line class="sparkz--cursor" x1="-100000" x2="-100000" y1="0" y2="30" stroke-width="2"></line><circle class="sparkz--spot" cx="-100000" cy="-100000" r="2"></circle><rect width="100" height="30" style="fill: transparent; stroke: transparent" class="sparkz--interaction-layer"></rect></svg>
 `
 
 exports['sparkz renders svg with custom cursor 1'] = `
